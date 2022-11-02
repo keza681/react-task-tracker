32c258111a27aaab771a3b4828bb7aa8f3d69c98
keza681 <lindakeza19@gmail.com>
2022-11-02 15:26:55 +0200

    Implement the add buttton to the header

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,15 +1,17 @@
+import Header from "./components/Header";
 
 function App() {
-  const name= ', Brad'
-  const x= false
+
 
 
   return (
-    <div className="App">
-      <h1>Hello From React  {name}</h1>
-      <h2>Hello {x ? 'Yes' : 'No'}</h2> 
+    <div className= 'container'>
+      <Header />
+      
     </div>
   )
 }
+
+
 
 export default App;
