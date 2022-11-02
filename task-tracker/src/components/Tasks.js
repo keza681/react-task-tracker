566ae907356afc9cc5e19128dca84b7bd398d414
keza681 <lindakeza19@gmail.com>
2022-11-02 21:23:47 +0200

    Implement the delete feature

--- task-tracker/src/components/Tasks.js
+++ task-tracker/src/components/Tasks.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import Task from "./Task"
 
-const Tasks = () => {
+const Tasks = ({tasks, onDelete}) => {
+  
+
   return (
-    <div>
+    <>
+    {tasks.map((task) => (
+      <Task key={task.id} task={task} onDelete={onDelete}/>
+    ))}
       
-    </div>
+    </>
   )
 }
 
