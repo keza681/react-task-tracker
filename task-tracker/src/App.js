95ba9554172d9e36558b292d057510a0e7aa3f47
keza681 <lindakeza19@gmail.com>
2022-11-03 21:17:57 +0200

    Implement Fetch data from the mockbackend feature

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,62 +1,50 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState ([
-    {
-      id:1,
-      text: 'Doctors Appointment',
-      day: 'Feb 5th at 2:30pm',
-      reminder: true,
-  
-    },
-    {
-      id:2,
-      text: 'Meeting at School',
-      day: 'Feb 6th at 1:30pm',
-      reminder: false,
-  
-    },
-    {
-      id:3,
-      text: 'Food Shopping',
-      day: 'Feb 5th at 2:30pm',
-      reminder: false,
-  
-    },
-    {
-      id:4,
-      text: 'Clothes Shopping',
-      day: 'Feb 6th at 1:30pm',
-      reminder: true,
-  
+  const [tasks, setTasks] = useState ([]);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
     }
-]);
+    getTasks()
+  }, [])
 
-// Add Task
-const addTask = (task) => {
-  const id= Math.floor(Math.random() * 
-  10000) + 1
-  const newTask = { id, ...task }
-  setTasks([...tasks, newTask])
-}
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch ('http://localhost:5000/tasks')
+    const data = await res.json()
 
-// Delete Task
-const deleteTask = (id) =>{
-  setTasks(tasks.filter((task) => task.id !==id))
-}
+    return data;  
+  }
 
-// Toggle Reminder
+
+  // Add Task
+  const addTask = (task) => {
+    const id= Math.floor(Math.random() * 
+    10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
+  // Delete Task
+  const deleteTask = (id) =>{
+    setTasks(tasks.filter((task) => task.id !==id))
+  }
+
+  // Toggle Reminder
   const toggleReminder = (id) => {
     setTasks
       (tasks.map((task) =>
         task.id === id ? { ...task, reminder:
         !task.reminder } : task
       )
-   )
+  )
   }
 
 
